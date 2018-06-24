99586ef0caec9fdf02ee0cc5b99fc1a504ecef32
petejos80 <petejos80@gmail.com>
2018-06-24 12:07:56 -0500

    Added scraping for New York times website

--- models/Article.js
+++ models/Article.js
@@ -7,23 +7,27 @@ var Schema = mongoose.Schema;
 // This is similar to a Sequelize model
 var ArticleSchema = new Schema({
   // `title` is required and of type String
-  title: {
-    type: String,
-    required: true
-  },
-  // `link` is required and of type String
-  link: {
-    type: String,
-    required: true
-  },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
-});
+    title: {
+      type: String,
+      required: true
+    },
+    summary: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    },
+    saved: {
+      type: Boolean,
+      default: false
+    },
+    notes: [{
+       type: Schema.Types.ObjectId,
+       ref: "Note"
+    }]
+  });
 
 // This creates our model from the above schema, using mongoose's model method
 var Article = mongoose.model("Article", ArticleSchema);
--- server.js
+++ server.js
@@ -2,10 +2,18 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-var axios = require("axios");
+
+
+// Tools for Scrping
+var request = require("request")
 var cheerio = require("cheerio");
 
+
+// DB Conntection
 var db = require("./models");
+
+// Include Article
+var Article = require("./models/Article.js");
 
 var PORT = 3000;
 
@@ -23,42 +31,45 @@ mongoose.connect("mongodb://localhost/week18Populater");
 
 // Routes
 
-// Scraping route
+// A GET request to scrape the New York Times website
 app.get("/scrape", function(req, res) {
-
-  axios.get("http://www.echojs.com/").then(function(response) {
+  // First, we grab the body of the html with request
+  request("https://www.nytimes.com/", function(error, response, html) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
-
+    var $ = cheerio.load(html);
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("article").each(function(i, element) {
+
       // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
+      // Add the title and summary of every link, and save them as properties of the result object
+      result.title = $(this).children("h2").text();
+      result.summary = $(this).children(".summary").text();
+      result.link = $(this).children("h2").children("a").attr("href");
 
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, send it to the client
-          return res.json(err);
-        });
+      // Using our Article model, create a new entry
+      // This effectively passes the result object to the entry (and the title and link)
+      var entry = new Article(result);
+
+      // Now, save that entry to the db
+      entry.save(function(err, doc) {
+        // Log any errors
+        if (err) {
+          console.log(err);
+        }
+        // Or log the doc
+        else {
+          console.log(doc);
+        }
+      });
+
     });
+        res.send("Scrape Complete");
 
-    // If we were able to successfully scrape and save an Article, send a message to the client
-    res.send("Scrape Complete");
   });
 });
+
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
