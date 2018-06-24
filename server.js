0433f518e400778148ee489a86d8965bc7c56fd5
petejos80 <petejos80@gmail.com>
2018-06-24 10:58:51 -0500

    Adding additional fields for scraper

--- server.js
+++ server.js
@@ -2,38 +2,30 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
 
-// Require all models
 var db = require("./models");
 
 var PORT = 3000;
 
-// Initialize Express
 var app = express();
 
-// Configure middleware
-
-// Use morgan logger for logging requests
 app.use(logger("dev"));
-// Use body-parser for handling form submissions
+
+// Body-parser for form submissions
 app.use(bodyParser.urlencoded({ extended: true }));
-// Use express.static to serve the public folder as a static directory
+
 app.use(express.static("public"));
 
-// Connect to the Mongo DB
+// MongoDB Connection
 mongoose.connect("mongodb://localhost/week18Populater");
 
 // Routes
 
-// A GET route for scraping the echoJS website
+// Scraping route
 app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with request
+
   axios.get("http://www.echojs.com/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
