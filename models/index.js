3a608a1930a1dca450b30c2005bd8066daa32939
petejos80 <petejos80@gmail.com>
2018-06-24 19:45:02 -0500

    Fixed issue with note delete not working correctly

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
-// Exporting an object containing all of our models
+// // Exporting an object containing all of our models
 
-module.exports = {
-    Article: require("./Article"),
-    Note: require("./Note")
-  };
+// module.exports = {
+//     Article: require("./Article"),
+//     Note: require("./Note")
+//   };
   
